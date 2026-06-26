f9e3b84c001ab406b1fdf4d5ca70b2122178a0ab
anterotero123 <anterotero123@gmail.com>
2026-06-26 13:12:59 +0300

    Update script.js
    test location fallback

--- script.js
+++ script.js
@@ -110,12 +110,18 @@ data.name || "Sijainti"
 
 ()=>{
 
+document.getElementById("city").textContent =
+"📍Haetaan verkosta...";
+
 
 fetch("https://ipapi.co/json/")
 
 .then(r=>r.json())
 
 .then(data=>{
+
+document.getElementById("city").textContent =
+`📍${data.city || "Ei kaupunkia"}`;
 
 
 loadWeather(
@@ -124,15 +130,13 @@ data.longitude,
 data.city || "Tuntematon"
 );
 
+})
+
+.catch(error=>{
+
+document.getElementById("city").textContent =
+"📍Sijaintivirhe";
 
 });
 
-
 }
-
-);
-
-}
-
-
-getLocation();
