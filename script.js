8175eda99739cd3adbf6dcdfd5470233bbbbc10d
anterotero123 <anterotero123@gmail.com>
2026-06-27 11:41:25 +0300

    Update script.js
    Add weather icons

--- script.js
+++ script.js
@@ -43,6 +43,42 @@ if (navigator.getBattery) {
 
 }
 
+function getWeatherIcon(code){
+
+    if(code === 0){
+        return "☀️";
+    }
+
+    if(code === 1 || code === 2){
+        return "🌤️";
+    }
+
+    if(code === 3){
+        return "☁️";
+    }
+
+    if(code >= 45 && code <= 48){
+        return "🌫️";
+    }
+
+    if(code >= 51 && code <= 67){
+        return "🌧️";
+    }
+
+    if(code >= 71 && code <= 77){
+        return "❄️";
+    }
+
+    if(code >= 80 && code <= 82){
+        return "🌦️";
+    }
+
+    if(code >= 95){
+        return "⛈️";
+    }
+
+    return "🌡️";
+}
 
 // SÄÄ
 
@@ -66,7 +102,7 @@ function loadWeather(lat, lon, city){
         );
 
         document.getElementById("weather").textContent =
-        `☀️${temp}°C`;
+`${getWeatherIcon(data.current_weather.weathercode)}${temp}°C`;
 
     });
 
